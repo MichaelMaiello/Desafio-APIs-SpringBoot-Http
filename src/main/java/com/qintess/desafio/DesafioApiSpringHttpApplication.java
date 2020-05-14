089e3ab4df347ac27ec9037a6d96c1c67034bbe2
MichaelMaiello <michael_maiello@hotmail.com>
2020-05-14 10:49:40 -0300

    Criação do projeto

---
+++ src/main/java/com/qintess/desafio/DesafioApiSpringHttpApplication.java
@@ -0,0 +1,13 @@
+package com.qintess.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiSpringHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiSpringHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qintess/desafio/DesafioApiSpringHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qintess.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiSpringHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
